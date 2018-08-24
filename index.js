7f27979f6bb78e0a3ac84f0fccdc39b69b89261e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 13:54:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+  function produceDrivingRange(blockRange){
+    return function(street1, street2){
+      const st1 = parseInt(street1,10);
+      const st2 = parseInt(street2,10);
+      const trip = Math.abs(st1-st2);
+      // const st2 = street2.replace(/\D/g,'');
+      if(blockRange > trip){
+        return `within range by ${blockRange-trip}`
+      } else {
+        return `${Math.abs(trip-blockRange)} blocks out of range`
+      }
+    }
+  }
+
+  function produceTipCalculator(pct){
+    const adjPct = pct;
+    return function(amt){
+      return amt *(adjPct);
+    }
+  }
+
+function createDriver(){
+  let driverId = 0;
+
+  return class{
+    constructor(name){
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
