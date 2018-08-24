853871523034edf54098d15febc470e1c2afd01f
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 13:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function produceDrivingRange(blockRange){
-  return function(street 1, street2){
+  return function(street1, street2){
+    let stree
     if(blockRange > trip){
       return `within range by ${blockRange-trip}`
     } else {
