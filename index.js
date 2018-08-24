fe25e8509a5809178e88b4e8887ef8e1b5d10a2e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 13:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange){
   return function(street1, street2){
   const st1 = parseInt(street1,10);
   const st2 = parseInt(street2,10);
-  const trip = Math.abs(st1-st2)
+  const trip = Math.abs(st1-st2);
   // const st2 = street2.replace(/\D/g,'');
     if(blockRange > trip){
       return `within range by ${blockRange-trip}`
