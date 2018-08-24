d2bc9d80e70e498def55c46d08e7159d1f1f2290
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 13:51:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function produceDrivingRange(blockRange){
-  return function(street1, street2){
-  const st1 = parseInt(street1,10);
-  const st2 = parseInt(street2,10);
-  const trip = Math.abs(st1-st2);
-  // const st2 = street2.replace(/\D/g,'');
-    if(blockRange > trip){
-      return `within range by ${blockRange-trip}`
-    } else {
-      return `${Math.abs(trip-blockRange)} blocks out of range`
+  function produceDrivingRange(blockRange){
+    return function(street1, street2){
+      const st1 = parseInt(street1,10);
+      const st2 = parseInt(street2,10);
+      const trip = Math.abs(st1-st2);
+      // const st2 = street2.replace(/\D/g,'');
+      if(blockRange > trip){
+        return `within range by ${blockRange-trip}`
+      } else {
+        return `${Math.abs(trip-blockRange)} blocks out of range`
+      }
     }
   }
-}
+
+  function produceTipCalculator(pct){
+    return function(amt){
+      return amt *(1+pct);
+    }
+  }
